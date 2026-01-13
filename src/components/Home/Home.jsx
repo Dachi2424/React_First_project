5ad09e079e65895cdcf51b0433753884ba3f179e
Dachi Darjania <dachidar19@gmail.com>
2026-01-13 20:06:29 +0400

    davaamte

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Hero from './Hero/Hero';
+import Second from './Second/Second';
 import './Home.scss'
+
 
 function Home(){
   return(
     <>
       <Hero />
+      <Second />
     </>
   );
 }
---
+++ src/components/Home/Second/Second.jsx
@@ -0,0 +1,11 @@
+import './Second.scss'
+
+function Second(){
+  return (
+    <section className="second">
+      
+    </section>
+  );
+}
+
+export default Second;
